d3fc872c554cfd22f5a6916dc33b2833858f6411
dilyin9 <dilyin9@gmail.com>
2022-09-05 00:57:22 +0300

    05/09

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", startpage)
+
+/*function startpage() {
+  let block = document.querySelector('.viewpager');
+  block.insertAdjacentHTML('afterbegin', '<p>Слайдер картинок</p>');
+}*/
